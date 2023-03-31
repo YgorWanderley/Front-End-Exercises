49771849351020678ecf5ec78b3660205db498a3
Ygor Wanderley <ygor.wanderley@hotmail.com>
2023-03-31 17:04:05 -0300

    primeiro react

---
+++ introducao-react/01.hello,world/exercicio-1/my-first-app/src/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Ygor Wanderley</h1>
+
+                <p>Sou da Paraíba, tenho 25 anos e sou iniciante em programação</p>;
+
+                <h2>Minhas Habilidades</h2>
+
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JAVASCRIPT</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default About;
