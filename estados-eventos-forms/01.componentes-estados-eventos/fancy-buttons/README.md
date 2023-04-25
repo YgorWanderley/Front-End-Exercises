0f1638cb0f18ff6205b46db488c5ce085ef6590c
Ygor Wanderley <ygor.wanderley@hotmail.com>
2023-04-25 10:00:17 -0300

    exercicio 2

--- estados-eventos-forms/01.componentes-estados-eventos/fancy-buttons/README.md
+++ estados-eventos-forms/01.componentes-estados-eventos/fancy-buttons/README.md
@@ -1,4 +1,11 @@
+<!-- exercicio 1 -->
 Crie uma aplicação React com npx create-react-app fancy-buttons, que atenda ao seguinte critérios: O componente App.jsdeve ser alterado para que seja um componente de classe e contenha um botão associado a um evento que imprime um texto qualquer via console.log().
 ⚠ Não se preocupe com a sintaxe correta para funções de eventos por enquanto.
 
 Faça com que sua aplicação anterior exiba três botões lado a lado com textos diferentes. Cada botão clicado deve acionar um evento diferente, cada um escrevendo algo diferente no console do navegador via console.log().
+
+<!-- exercicio 2 -->
+Utilizando o mesmo código do exercício anterior, declare dentro da classe <App> as funções que, anteriormente, foram declaradas fora da classe.
+
+Faça com que as funções criadas no exercício anterior possuam acesso ao objeto this.
+
--- estados-eventos-forms/01.componentes-estados-eventos/fancy-buttons/src/App.js
+++ estados-eventos-forms/01.componentes-estados-eventos/fancy-buttons/src/App.js
@@ -1,22 +1,26 @@
 import React from 'react'
 
-const handleButtonOne = () => {
-  console.log('Clicou no botão 1');
-}
-const handleButtonTwo = () => {
-  console.log('Clicou no botão 2');
-}
-const handleButtonThree = () => {
-  console.log('Clicou no botão 3');
-}
 
 class App extends React.Component{
+  constructor() {
+    super()
+  this.handleButtonOne = this.handleButtonOne.bind(this)
+  }
+  handleButtonOne() {
+    console.log('Clicou no botão 1', this);
+  }
+  handleButtonTwo = () => {
+    console.log('Clicou no botão 2');
+  }
+  handleButtonThree = () => {
+    console.log('Clicou no botão 3');
+  }
   render() {
     return (
       <>
-      <button onClick={ handleButtonOne }>Botão 1</button>
-      <button onClick={ handleButtonTwo }>Botão 2</button>
-      <button onClick={ handleButtonThree }>Botão 3</button>
+      <button onClick={ this.handleButtonOne }>Botão 1</button>
+      <button onClick={ this.handleButtonTwo }>Botão 2</button>
+      <button onClick={ this.handleButtonThree }>Botão 3</button>
       </>
     )
   }
