784c598bf927a1dbbaf94d87c5da4e98f9729090
Ygor Wanderley <ygor.wanderley@hotmail.com>
2023-04-25 11:10:44 -0300

    exercicio 4

--- estados-eventos-forms/01.componentes-estados-eventos/fancy-buttons/src/App.js
+++ estados-eventos-forms/01.componentes-estados-eventos/fancy-buttons/src/App.js
@@ -9,6 +9,10 @@ class App extends React.Component{
     clicksBtnThree: 0,
   };
 
+  getButtonColor(num) {
+    return num % 2 === 0 ? 'green' : 'white';
+  }
+
   handleButtonOne = () => {
     this.setState((prevState) => ({
       clicksBtnOne: prevState.clicksBtnOne + 1,
@@ -32,16 +36,21 @@ class App extends React.Component{
       <button
       type='button'
       onClick={ this.handleButtonOne }
+      style={ { backgroundColor: this.getButtonColor(clicksBtnOne) } }
       >
         {`Cliques botão 1: ${clicksBtnOne}`}
       </button>
       <button
+      type='button'
       onClick={ this.handleButtonTwo }
+      style={ { backgroundColor: this.getButtonColor(clicksBtnTwo) } }
       >
         {`Cliques botão 2: ${clicksBtnTwo}`}
       </button>
       <button
+      type='button'
       onClick={ this.handleButtonThree }
+      style={ { backgroundColor: this.getButtonColor(clicksBtnThree) } }
       >
         {`Cliques botão 3: ${clicksBtnThree}`}
       </button>
